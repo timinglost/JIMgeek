4f42a098a98aa6da6489b0f7f5f15d30dc26e419
Kirill <budanovkirilldmitrievich@gmail.com>
2022-01-26 17:14:32 +0300

    dz

--- client.py
+++ client.py
@@ -96,28 +96,61 @@ def main():
         config = load_config('config.yaml')
         config_message = load_config('config_massag.yaml')
         try:
-                address = sys.argv[1]
-                port = int(sys.argv[2])
+                if '-p' in sys.argv:
+                        port = int(sys.argv[sys.argv.index('-p') + 1])
+                else:
+                        port = config['DEFAULT_PORT']
+                # address = sys.argv[1]
+                # port = int(sys.argv[2])
                 if not 65535 >= port >= 1024:
                         raise ValueError
+        # except IndexError:
+        #         address = config['DEFAULT_IP_ADDRESS']
+        #         port = config['DEFAULT_PORT']
         except IndexError:
-                address = config['DEFAULT_IP_ADDRESS']
-                port = config['DEFAULT_PORT']
+                client_log.warning('После -\'p\' необходимо указать порт')
+                sys.exit(1)
         except ValueError:
                 client_log.warning('Порт должен быть указан в пределах от 1024 до 65535')
+                sys.exit(1)
+        try:
+                if '-a' in sys.argv:
+                        address = sys.argv[sys.argv.index('-a') + 1]
+                else:
+                        address = config['DEFAULT_IP_ADDRESS']
+
+        except IndexError:
+                client_log.warning('После \'a\'- необходимо указать адрес для ')
+                sys.exit(1)
+        try:
+                if '-rw' in sys.argv:
+                        console = int(sys.argv[sys.argv.index('-rw') + 1])
+                else:
+                        console = 1
+        except IndexError:
+                client_log.warning('После \'rw\'- необходимо указать команду на чтение(0) или на запись (1)')
+                sys.exit(1)
+        try:
+                if '-name' in sys.argv:
+                        client_name = sys.argv[sys.argv.index('-name') + 1]
+                else:
+                        client_name = 'anonymous'
+        except IndexError:
+                client_log.warning('После \'name\'- необходимо указать имя')
                 sys.exit(1)
         s = socket(AF_INET, SOCK_STREAM)
         try:
                 s.connect((address, port))
                 client_log.info('Установленно подключение')
-                client_name = input('Введите имя: ')
-
-                receiver = threading.Thread(target=message_from_server, args=(s, client_name, config_message, config))
-                receiver.daemon = True
-                receiver.start()
-
-                user_interface = threading.Thread(target=massage_post, args=(s, client_name, config_message, config))
-                user_interface.start()
+                # client_name = input('Введите имя: ')
+                if console == 0:
+                        receiver = threading.Thread(target=message_from_server, args=(s, client_name, config_message, config))
+                        # receiver.daemon = True
+                        receiver.start()
+                if console == 1:
+                        user_interface = threading.Thread(target=massage_post, args=(s, client_name, config_message, config))
+                        # receiver.daemon = True
+                        user_interface.start()
         except ConnectionRefusedError:
                 client_log.error('400: Bad Request')
 
---
+++ task_4.py
@@ -0,0 +1,6 @@
+import subprocess
+count = int(input('Введите количество: '))
+for i in range(count):
+    name = input('Введите имя: ')
+    subprocess.Popen(['python', 'client.py', '-rw', '0', '-name', f'{name}'], creationflags=subprocess.CREATE_NEW_CONSOLE)
+    subprocess.Popen(['python', 'client.py', '-rw', '1', '-name', f'{name}'], creationflags=subprocess.CREATE_NEW_CONSOLE)

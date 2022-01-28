1ddc1afcf16540dfccf3dced4ea50c9157ecde95
Kirill <budanovkirilldmitrievich@gmail.com>
2022-01-28 18:28:48 +0300

    dz

--- client.py
+++ client.py
@@ -45,29 +45,43 @@ def response_treatment(answer, config):
 def message_from_server(sock, username, m_config, config):
         while True:
             message = get_data(sock, config)
+            if m_config['ACTION'] in message and message[m_config['ACTION']] == m_config['JOIN'] and \
+                    m_config['FROM'] in message:
+                    print(f'{message[m_config["FROM"]]} присоединился к чату.')
             if m_config['ACTION'] in message and message[m_config['ACTION']] == 'msg' and \
                 m_config['FROM'] in message and m_config['MESSAGE'] in message and \
                     m_config['TIME'] in message:
-                    print(f'\n{message[m_config["FROM"]]}: {message[m_config["MESSAGE"]]}')
-            if config['ACTION'] in message and message[config['ACTION']] == 'leave' and \
+                    print(f'{message[m_config["FROM"]]}: {message[m_config["MESSAGE"]]}')
+            if config['ACTION'] in message and message[config['ACTION']] == m_config['LEAVE'] and \
                     config['TIME'] in message:
-                    print(f'\n{message[config["ACCOUNT_NAME"]]} покинул чат.')
+                    print(f'{message[m_config["ACCOUNT_NAME"]]} покинул чат.')
 
 
 
 @log
 def massage_post(sock, username, m_config, config):
+        create_join_message(sock, username, m_config, config)
+        print(f'Добро пожаловать в чат {username}!')
         while True:
-                command = input('Введите команду: ')
+                command = input('Введите команду "m" для сообщения, "e" для выхода: ')
                 if command == 'm':
                         create_message(sock, username, m_config, config)
                 elif command == 'e':
-                        post_data(sock, create_exit_message(username, config), m_config)
+                        create_exit_message(sock, username, m_config, config)
                         print('Завершение соединения.')
-                        time.sleep(0.5)
                         break
                 else:
                         print('Команда не распознана, попробойте снова.')
+
+
+@log
+def create_join_message(sock, username, m_config, config):
+        message_dict = {
+                m_config['ACTION']: m_config['JOIN'],
+                m_config['TIME']: time.time(),
+                m_config['FROM']: username
+        }
+        post_data(sock, message_dict, config)
 
 
 @log
@@ -83,12 +97,13 @@ def create_message(sock, username, m_config, config):
 
 
 @log
-def create_exit_message(name, config):
-        return {
-                config['ACTION']: 'leave',
+def create_exit_message(sock, username, m_config, config):
+        message_dict = {
+                config['ACTION']: m_config['LEAVE'],
                 config['TIME']: time.time(),
-                config['ACCOUNT_NAME']: name
+                config['ACCOUNT_NAME']: username
         }
+        post_data(sock, message_dict, config)
 
 
 def main():
--- task_4.py
+++ task_4.py
@@ -2,5 +2,7 @@ import subprocess
 count = int(input('Введите количество: '))
 for i in range(count):
     name = input('Введите имя: ')
+    if name == '':
+        name = f'anonymous_{i}'
     subprocess.Popen(['python', 'client.py', '-rw', '0', '-name', f'{name}'], creationflags=subprocess.CREATE_NEW_CONSOLE)
     subprocess.Popen(['python', 'client.py', '-rw', '1', '-name', f'{name}'], creationflags=subprocess.CREATE_NEW_CONSOLE)
